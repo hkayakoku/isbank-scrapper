f51203a34cb28409ad5fdc021a1290ed1cae58bd
Hakan Kayakoku <hkayakoku@internet.milsoft.com.tr>
2021-11-24 14:38:27 +0300

    some update

--- main.py
+++ main.py
@@ -13,17 +13,19 @@ old_curr = {"us": 0.0, "eur": 0.0, "gbp": 0.0}
 
 while True:
     res = requests.get(request_mig)
+    update = False
     for key, curr_item in curr_dict.items():
         html_content = BeautifulSoup(res.content, "lxml")
         tr_tag = html_content.find("tr", {"id": curr_item})
         split_list = tr_tag.text.split()
         curr_alis = float(split_list[-1].replace(',', '.'))
-        update = False
+        curr_satis = float(split_list[-2].replace(',', '.'))
         if old_curr[key] != curr_alis:
             old_curr[key] = curr_alis
             update = True
         if update:
-            print(" ".join(tr_tag.text.split()))
+            print("{} diff: {:.3f}".format(" ".join(tr_tag.text.split()), curr_satis-curr_alis))
+    if update:
+        print("-------------------")
 
-    print("----------------------")
     time.sleep(2.5)
